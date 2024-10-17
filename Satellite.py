528349336dc8e497eeb1b66fc0725a1b9995e4bf
SebastianMagnusson <sebmag-1@student.ltu.se>
2024-10-17 12:04:20 +0200

    Added a receiver, so now we have fullscreen terminals mode yes

--- Satellite.py
+++ Satellite.py
@@ -4,26 +4,33 @@ import time
 
 def run_server():
 
-    server1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    groundsender = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    payloadserver = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    groundreciever = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     server_ip = "127.0.0.1"
-    port1 = 8000
-    port2 = 9000
+    groundsendport = 8000
+    payloadport = 9000
+    groundrecieveport = 10000
 
-    server1.bind((server_ip, port1))
-    server2.bind((server_ip, port2))
+    groundsender.bind((server_ip, groundsendport))
+    payloadserver.bind((server_ip, payloadport))
+    groundreciever.bind((server_ip, groundrecieveport))
 
-    server1.listen(0)
-    print(f"Listening on {server_ip}:{port1}")
-    server2.listen(0)
-    print(f"Listening on {server_ip}:{port2}")
+    groundsender.listen(0)
+    print(f"Listening on {server_ip}:{groundsendport}")
+    payloadserver.listen(0)
+    print(f"Listening on {server_ip}:{payloadport}")
+    groundreciever.listen(0)
+    print(f"Listening on {server_ip}:{groundrecieveport}")
 
     # accept incoming connections
-    client_socket1, client_address1 = server1.accept()
+    client_socket1, client_address1 = groundsender.accept()
     print(f"Accepted connection from {client_address1[0]}:{client_address1[1]}")
-    client_socket2, client_address2 = server2.accept()
+    client_socket2, client_address2 = payloadserver.accept()
     print(f"Accepted connection from {client_address2[0]}:{client_address2[1]}")
+    client_socket3, client_address3 = groundreciever.accept()
+    print(f"Accepted connection from {client_address3[0]}:{client_address3[1]}")
 
     while True:
         request1 = client_socket1.recv(1024)
@@ -36,6 +43,7 @@ def run_server():
             # connection should be closed and break out of the loop
             client_socket1.send("closed".encode("utf-8"))
             client_socket2.send("closed".encode("utf-8"))
+            client_socket3.send("closed".encode("utf-8"))
             break
 
         print(f"Received: {request1} at {time.localtime()[3]}:{time.localtime()[4]}:{time.localtime()[5]}")
@@ -50,6 +58,7 @@ def run_server():
             response1 = "not a valid command srry man".encode("utf-8")
             response2 = "the fuck u on about".encode("utf-8")
         client_socket1.send(response1)
+        client_socket3.send(response1)
         client_socket2.send(response2)
 
     # close connection socket with the client
@@ -57,9 +66,12 @@ def run_server():
     print("Connection to client1 closed")
     client_socket2.close()
     print("Connection to client2 closed")
+    client_socket3.close()
+    print("Connection to client3 closed")
     # close server1 socket
-    server1.close()
-    server2.close()
+    groundsender.close()
+    payloadserver.close()
+    groundreciever.close()
 
 
 run_server()

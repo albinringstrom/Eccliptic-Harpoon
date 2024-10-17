54af44231d5c809e24110c4d442d2ec45c672fbc
joeott-1 <joeott-1@student.ltu.se>
2024-10-17 17:33:32 +0200

    Reapply "minor change"
    This reverts commit 52c896f534dc650209a483f9bb97d0d8f3049fae.

--- Ground_Station_Transmitter.py
+++ Ground_Station_Transmitter.py
@@ -28,7 +28,7 @@ def run_client():
 
     # close client socket (connection to the server)
     client.close()
-    print("Connection to server closed")
+    print("Connection to server closed rn")
 
 
 run_client()
